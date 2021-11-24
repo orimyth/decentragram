323dafd251a25463b403c54cf2c11cd260a88925
Shayan <shayan.nosrat@t-online.de>
2021-11-24 21:07:24 +0100

    adds deploy migration, adds basic uploadImage function and test

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+  // store images
+  uint public imageCount = 0;
+  mapping(uint => Image) public images;
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+  // create images
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    // increment image id
+    imageCount++;
+    // add image to contract
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+  }
+
+
+  // tip images
 }
